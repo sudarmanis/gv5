5a1c21f48bceb6e8d02381ab8dc840a9df9bc380
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-22 08:16:10 -0700

    Create n.java

---
+++ n.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class n
+{
+public static void main(String argS[])
+{
+int b;
+Scanner sc=new Scanner(System.in);
+b=sc.nextInt();
+for(int i=b;i==b;i++)
+{
+System.out.println(i+1);
+}
+}
+}
