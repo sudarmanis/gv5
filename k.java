eb7c069b4bc12b31ec1410b6cc5f197e2d623987
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-22 08:05:52 -0700

    Create k.java

---
+++ k.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class k
+{
+public static void main(String argS[])
+{
+int b;
+String s;
+Scanner sc=new Scanner(System.in);
+s=sc.nextLine();
+b=sc.nextInt();
+for(int i=1;i<=b;i++)
+{
+System.out.println(s);
+}
+}
+}
