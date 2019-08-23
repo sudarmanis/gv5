ccd56bc04c3cbdcdedc780d84c1f97c294a6b1c7
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-23 03:23:56 -0700

    Create ra.java

---
+++ ra.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class ra
+{
+public static void main(String argS[])
+{
+int b;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no");
+b=sc.nextInt();
+if((b>1)&&(b<9))
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
